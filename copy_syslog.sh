e3cd5abf4cd0ad181033090209ee5365c027e423
Michael <plaskettmichael@gmail.com>
2023-08-30 05:01:21 +0000

    day_time

---
+++ copy_syslog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script Name:                  
+# Author:                       Michael Plaskett    
+# Date of latest revision:      07/25/2023
+# Purpose:                     
+
+# Define the source and destination filenames
+source_file="/var/log/syslog"
+current_date=$(date +"%Y-%m-%d_%H-%M-%S")
+destination_file="syslog_${current_date}.log"
+
+# Copy the source file to the current working directory
+cp "$source_file" "$destination_file"
+
+echo "Copied $source_file to $destination_file"
