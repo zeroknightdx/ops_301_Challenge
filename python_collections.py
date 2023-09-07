584395c53d386582946aad370b7053315c78fd93
Michael <plaskettmichael@gmail.com>
2023-09-07 15:36:37 +0000

    somthing_new

---
+++ python_collections.py
@@ -0,0 +1,20 @@
+# Script Name:                 python collections
+# Author:                       Michael Plaskett    
+# Date of latest revision:      09/06/23
+# Purpose:          TO grasp the fundamental of data structures and algorithms
+# Resource: 
+
+# Assign a list of ten string elements to a variable
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+
+# Print the fourth element of the list (index 3)
+print("Fourth element:", my_list[3])
+
+# Print the sixth through tenth element of the list (index 5 to 9)
+print("Sixth through tenth elements:", my_list[5:10])
+
+# Change the value of the seventh element to "onion" (index 6)
+my_list[6] = "onion"
+
+# Print the updated list
+print("Updated list:", my_list)
